a0aa22b70713df32876f4fcd59d94c7a71d8aad6
gbrisc <gabrisc427@gmail.com>
2024-09-17 16:22:08 +0200

    Avances con la Vista (problemas con el JTextField)

--- MM/src/main/java/moneymanager/vista/VistaVentana.java
+++ MM/src/main/java/moneymanager/vista/VistaVentana.java
@@ -26,24 +26,25 @@ public class VistaVentana extends JFrame {
         setLayout(new BorderLayout());
 
         // PANEL PRINCIPAL
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
+        JPanel panelPrincipal = new JPanel();
+        panelPrincipal.setLayout(new BorderLayout());
 
-        // ETIQUETA NOMBRE DE LA CUENTA
-        /*
-        CAMBIARLO POR UN BOTÓN QUE AL HACERLO CLICK ENTRE EN EL MENU DE PODER MODIFICAR LA CUENTA.
-         */
+        // BOTÓN NOMBRE DE LA CUENTA
         String nombreCuenta = CM.getNombreCuenta();
-        JLabel etiquetaNombreCuenta = new JLabel("ACCOUNT", SwingConstants.CENTER);
-        etiquetaNombreCuenta.setFont(new Font("Lexend", Font.BOLD, 25));
-        etiquetaNombreCuenta.setBorder(new EmptyBorder(25,15,0,15));
-        panel.add(etiquetaNombreCuenta, BorderLayout.NORTH);
+        JButton botonNombreCuenta = new JButton("ACCOUNT");
+        botonNombreCuenta.setFont(new Font("Lexend", Font.BOLD, 30));
+        botonNombreCuenta.setBorder(new EmptyBorder(25,15,0,15));
+        botonNombreCuenta.setForeground(new Color(164, 227, 111 ));
+        botonNombreCuenta.setBackground(new Color(253,242,240));
+        panelPrincipal.add(botonNombreCuenta, BorderLayout.NORTH);
+
 
         // ETIQUETA CON EL SALDO DE LA CUENTA
         String saldo = CM.getSaldo();
         JLabel etiquetaSaldo = new JLabel(saldo, SwingConstants.CENTER);
         etiquetaSaldo.setFont(new Font("Lexend", Font.BOLD, 70));
-        panel.add(etiquetaSaldo, BorderLayout.CENTER);
+        etiquetaSaldo.setBorder(new EmptyBorder(0,15,50,15));
+        panelPrincipal.add(etiquetaSaldo, BorderLayout.CENTER);
 
 
 
@@ -54,43 +55,89 @@ public class VistaVentana extends JFrame {
         menuPanel.setPreferredSize(new Dimension(173, 0));
 
 
-        // BOTON REALIZAR OPERACION
-        JButton boton = new JButton("REALIZAR OPERACIÓN");
-        menuPanel.add(boton);
+        // BOTONES MENÚ
+        JButton realizarOperacion = new JButton("REALIZAR OPERACIÓN");
+        menuPanel.add(realizarOperacion);
 
-        JButton boton2 = new JButton("VER HISTORIAL");
-        menuPanel.add(boton2);
+        JButton verHistorial = new JButton("VER HISTORIAL");
+        menuPanel.add(verHistorial);
 
-        JButton boton3 = new JButton("CUENTAS");
-        menuPanel.add(boton3);
+        JButton consultarCuentas = new JButton("CUENTAS");
+        menuPanel.add(consultarCuentas);
+
+        this.add(menuPanel, BorderLayout.WEST); // Menú en el lado izquierdo
+        this.add(panelPrincipal, BorderLayout.CENTER); // Contenido principal en el centro
+
+        setVisible(true);
+
+        // PANEL MODIFICAR CUENTA
+        JPanel panelModificarCuenta = new JPanel();
+        panelModificarCuenta.setLayout(new BorderLayout());
+        panelModificarCuenta.setVisible(false);
+
+        // ETIQUETA CUENTA
+        JLabel etiquetaNombreCuenta = new JLabel("ACCOUNT", SwingConstants.CENTER);
+        etiquetaNombreCuenta.setFont(new Font("Lexend", Font.BOLD, 30));
+        etiquetaNombreCuenta.setBorder(new EmptyBorder(25,15,15,15));
+        etiquetaNombreCuenta.setForeground(new Color(164, 227, 111 ));
+        panelModificarCuenta.add(etiquetaNombreCuenta, BorderLayout.NORTH);
+
+        // CONTENIDO PANEL
+
+        // CAMBIO NOMBRE
+
+        JPanel cambioNombre = new JPanel();
+        cambioNombre.setLayout(new BorderLayout());
+        cambioNombre.setVisible(true);
 
 
-        // Evento de acción para el botón
-        boton.addActionListener(new ActionListener() {
+
+        JLabel etiquetaCambiarNombre = new JLabel("CAMBIAR NOMBRE:", SwingConstants.LEFT);
+        etiquetaCambiarNombre.setFont(new Font("Lexend", Font.BOLD, 15));
+        etiquetaCambiarNombre.setBorder(new EmptyBorder(25,15,15,15));
+        etiquetaCambiarNombre.setForeground(new Color(164, 227, 111 ));
+        cambioNombre.add(etiquetaCambiarNombre, BorderLayout.NORTH);
+
+        JTextField nuevoNombre = new JTextField(20);
+        nuevoNombre.setToolTipText("NUEVO NOMBRE AQUÍ");
+        nuevoNombre.setFont(new Font("Lexend", Font.BOLD, 15));
+        nuevoNombre.setBorder(null);
+        nuevoNombre.setForeground(new Color(253,242,240));
+        nuevoNombre.setBackground(new Color(164, 227, 111 ));
+        cambioNombre.add(nuevoNombre, BorderLayout.CENTER);
+
+        cambioNombre.setBorder(new EmptyBorder(20,20,20,20));
+
+        panelModificarCuenta.add(cambioNombre, BorderLayout.CENTER);
+
+
+        // VENTANA
+
+        this.add(menuPanel, BorderLayout.WEST); // Menú en el lado izquierdo
+        this.add(panelPrincipal, BorderLayout.CENTER); // Contenido principal en el centro
+        this.add(panelModificarCuenta, BorderLayout.CENTER);
+
+
+        // FUNCIONES BOTONES
+
+        botonNombreCuenta.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Aquí puedes llamar a la lógica de negocio o cambiar la ventana
-                JOptionPane.showMessageDialog(null, "Botón presionado");
+                panelPrincipal.setVisible(false);
+                panelModificarCuenta.setVisible(true);
             }
         });
 
-
-        this.add(menuPanel, BorderLayout.WEST); // Menú en el lado izquierdo
-        this.add(panel, BorderLayout.CENTER); // Contenido principal en el centro
-
-        // Hacer visible la ventana
-        setVisible(true);
     }
 
-    public static void cambiarEstilo(){
+    private static void cambiarEstilo(){
         UIManager.put("Button.background", new Color( 164, 227, 111 ));
         UIManager.put("Button.foreground", new Color(253,242,240));
         UIManager.put("Button.font", new Font("Lexend", Font.BOLD, 12));
-        UIManager.put("Button.border", BorderFactory.createEmptyBorder(15, 15, 15,15));
+        UIManager.put("Button.border", BorderFactory.createEmptyBorder(10, 15, 10,15));
         UIManager.put("Button.focus", new Color(0,0,0,0));
-        UIManager.put("Button.select", new Color( 102, 209, 180 ));
-        UIManager.put("Button.foreground", new Color(253,242,240));
-
+        UIManager.put("Button.select", new Color( 253,242,240));
+        UIManager.put("Button.foreground", new Color(35, 33, 33));
         UIManager.put("Panel.background", new Color(253,242,240));
     }
 
